4dc5e82a6e20127cf7a7e531e7b550e059e06408
Ben Douadia <simo2332001@yahoo.fr>
2018-07-02 12:34:19 +0200

    rr

---
+++ bin/features/RegelnBedingungenAnlegen.feature
@@ -0,0 +1,11 @@
+Feature: Regeln
+
+  Scenario: Regeln Anlegen
+    Given Ich bin als Admin eingeloggt
+    Given Ich rufe die Regeln auf
+    And Ich rufe die Maske (Neu) auf
+    When Ich fuege die Daten ein
+    And Ich klicke Speichern
+    Then Ich sehe die Uebersicht Regeln
+    And Die angelegte Regeln ist selektiert
+    And Die eingegebenen Daten sind vorhanden
--- src/stepdefinitions/Login.java
+++ src/stepdefinitions/Login.java
@@ -6,15 +6,15 @@ import cucumber.api.java.en.Given;
 import utility.Hook;
 
 public class Login {
-//	private WebDriver driver;
-//	public Login() {
-//		this.driver = Hook.getDriver();
-//	}
+	private WebDriver driver;
+	public Login() {
+		this.driver = Hook.getDriver();
+	}
 
-//	@Given("^Ich bin als Admin eingeloggt$")
-//	public void ich_bin_als_Admin_eingeloggt() throws Throwable {
-//	    // Write code here that turns the phrase above into concrete actions
-//		driver.get("http://localhost:9091/insuria-commission-web-war");
-////		System.out.println("Current url : " + driver.getCurrentUrl());
-//	}
+	@Given("^Ich bin als Admin eingeloggt$")
+	public void ich_bin_als_Admin_eingeloggt() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.get("http://localhost:9091/insuria-commission-web-war");
+//		System.out.println("Current url : " + driver.getCurrentUrl());
+	}
 }
